935e1cf5e354e128462c98ff065ada166ee93951
John Melody Me and Tan Sin Dee <johnmelodyme@icloud.com>
2020-03-12 13:41:26 +0800

    Completed

--- app/src/main/java/my/com/kylogger/johnmelodyme/internet/locationtracker/LocationActivity.java
+++ app/src/main/java/my/com/kylogger/johnmelodyme/internet/locationtracker/LocationActivity.java
@@ -43,7 +43,7 @@ import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class LocationActivity extends AppCompatActivity {
     private static final String TAG = "GPSTRACKER";
-    private final static int ALL_PERMISSIONS_RESULT = 101;
+    private static final int ALL_PERMISSIONS_RESULT = 0x65;
     public static LocationTrack locationTrack;
     private ArrayList permissionsToRequest;
     private ArrayList permissionsRejected = new ArrayList();
